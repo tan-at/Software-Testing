487fd312fa45d20d41712c36c07b86f19bb1c494
Atte Tanskanen <94180117+tan-at@users.noreply.github.com>
2023-12-07 14:04:20 +0200

    Initial test for baseDifference.js

---
+++ test/baseDifference.test.js
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import baseDifference from "../src/.internal/baseDifference"; //
+
+describe("baseDifference", () => {
+  it("should return the difference of two arrays", () => {
+    const array = [1, 2, 3, 4, 5];
+    const values = [3, 5, 7];
+    const result = baseDifference(array, values);
+
+    expect(result).to.deep.equal([1, 2, 4]);
+  });
+
+  it("should handle empty arrays", () => {
+    const array = [];
+    const values = [1, 2, 3];
+    const result = baseDifference(array, values);
+
+    expect(result).to.deep.equal([]);
+  });
+
+  it("should handle arrays with objects and custom iteratee", () => {
+    const array = [{ x: 1 }, { x: 2 }, { x: 3 }];
+    const values = [{ x: 2 }];
+    const result = baseDifference(array, values, (obj) => obj.x);
+
+    expect(result).to.deep.equal([{ x: 1 }, { x: 3 }]);
+  });
+});
