64852f8ca45d731bc8c9aa2aebb55bc17f450224
Atte Tanskanen <94180117+tan-at@users.noreply.github.com>
2023-12-07 14:17:35 +0200

    Initial test fo baseIntersecrion.js

---
+++ test/baseIntersection.test.js
@@ -0,0 +1,36 @@
+import { expect } from "chai";
+import baseIntersection from "../src/.internal/baseIntersection";
+
+describe("baseIntersection", () => {
+  it("should return the intersection of arrays", () => {
+    const arrays = [
+      [1, 2, 3],
+      [2, 3, 4],
+      [3, 4, 5],
+    ];
+
+    const result = baseIntersection(arrays);
+
+    expect(result).to.deep.equal([3]);
+  });
+
+  it("should handle empty arrays", () => {
+    const arrays = [[1, 2, 3], [], [4, 5, 6]];
+
+    const result = baseIntersection(arrays);
+
+    expect(result).to.deep.equal([]);
+  });
+
+  it("should handle arrays with objects and custom iteratee", () => {
+    const arrays = [
+      [{ x: 1 }, { x: 2 }, { x: 3 }],
+      [{ x: 2 }, { x: 3 }, { x: 4 }],
+      [{ x: 3 }, { x: 4 }, { x: 5 }],
+    ];
+
+    const result = baseIntersection(arrays, (obj) => obj.x);
+
+    expect(result).to.deep.equal([{ x: 3 }]);
+  });
+});
