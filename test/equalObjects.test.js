db171bba3dba5425a031953043b69c5a029ca23b
Erfanniks <34509377+Erfanniks@users.noreply.github.com>
2023-12-07 12:18:59 +0200

    Create equalObjects.test.js
    tests for equalObjects.js

---
+++ test/equalObjects.test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const expect = chai.expect;
+const equalObjects = require('../src/.internal/equalObjects.js');
+
+describe('equalObjects', function() {
+  it('should return true for two empty objects', function() {
+    const obj1 = {};
+    const obj2 = {};
+    expect(equalObjects(obj1, obj2)).to.be.true;
+  });
+
+  it('should return false for objects with different properties', function() {
+    const obj1 = { a: 1 };
+    const obj2 = { b: 1 };
+    expect(equalObjects(obj1, obj2)).to.be.false;
+  });
+
+  it('should return true for identical objects', function() {
+    const obj1 = { a: 1, b: { c: 2 } };
+    const obj2 = { a: 1, b: { c: 2 } };
+    expect(equalObjects(obj1, obj2)).to.be.true;
+  });
+});
